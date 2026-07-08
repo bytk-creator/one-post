2313f82cc425837197cfd2342b3d9b71a9b519e1
bytk-creator <novyjk10@gmail.com>
2026-07-08 22:46:30 +0700

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,28 @@
-const CACHE_NAME = 'one-post-v1';
-const ASSETS = ['/', '/index.html', '/css/style.css', '/js/app.js'];
+const CACHE_NAME = 'one-post-v2';
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+    self.skipWaiting();
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((c) => c.addAll(['/', '/css/style.css', '/js/app.js']))
+    );
 });
 
 self.addEventListener('activate', (e) => {
-    e.waitUntil(caches.keys().then((k) => Promise.all(k.filter((x) => x !== CACHE_NAME).map((x) => caches.delete(x)))));
+    e.waitUntil(
+        caches.keys().then((k) => Promise.all(k.filter((x) => x !== CACHE_NAME).map((x) => caches.delete(x))))
+    );
+    return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
     if (e.request.url.includes('/api/') || e.request.url.includes('/uploads/')) return;
-    e.respondWith(caches.match(e.request).then((c) => c || fetch(e.request)));
+    e.respondWith(
+        fetch(e.request)
+            .then((response) => {
+                const clone = response.clone();
+                caches.open(CACHE_NAME).then((c) => c.put(e.request, clone));
+                return response;
+            })
+            .catch(() => caches.match(e.request))
+    );
 });
